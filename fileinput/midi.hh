27edd014740aa2b29f561999c730d5ccb6019a10
peterzhu9210 <peterzhu9210@gmail.com>
2017-05-01 14:44:52 -0400

    Read An midi file

---
+++ fileinput/midi.hh
@@ -0,0 +1,26 @@
+#ifndef MIDI_HH
+#define MIDI_HH
+#include<vector>
+#include<string>
+#include <QFile>
+
+using namespace std;
+
+
+
+vector<unsigned char> Head_Data;
+
+
+vector<unsigned char> Track_Data;
+vector<unsigned char> Track_Length;
+vector<unsigned char> Track_event;
+vector<unsigned char> Meta_Type;
+vector<unsigned char> Meta_Length;
+vector<unsigned char> Meta_event;
+vector<unsigned char> Nota_event;
+
+
+
+
+#endif // MIDI_HH
+
