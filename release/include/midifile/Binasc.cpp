642962261870e57af1bce6e6b3be0b3e9ff77050
peterzhu9210 <ezhu2@stevens.edu>
2017-05-07 16:01:43 -0400

    Convert bytes

--- release/include/midifile/Binasc.cpp
+++ release/include/midifile/Binasc.cpp
@@ -1,6 +1,6 @@
-+// Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
- +// Creation Date: Mon Feb 16 12:26:32 PST 2015 Adapted from binasc program.
- +// Last Modified: Wed Feb 18 14:48:21 PST 2015
+// Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
+// Creation Date: Mon Feb 16 12:26:32 PST 2015 Adapted from binasc program.
+// Last Modified: Wed Feb 18 14:48:21 PST 2015
 // Filename:      midifile/src-library/Binasc.cpp
 // Syntax:        C++11
 // vim:           ts=3 expandtab
