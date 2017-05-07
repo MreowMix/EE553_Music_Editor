074a8484b2e4fe2139a61fcec59ee5705c52da96
peterzhu9210 <ezhu2@stevens.edu>
2017-05-07 15:44:54 -0400

    Stores a MidiMessage

--- release/include/midifile/MidiEvent.cpp
+++ release/include/midifile/MidiEvent.cpp
@@ -1,7 +1,3 @@
-//
-// Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
-// Creation Date: Sat Feb 14 21:40:14 PST 2015
-// Last Modified: Sat Feb 14 23:33:51 PST 2015
 // Filename:      midifile/src/MidiEvent.cpp
 // Website:       http://midifile.sapp.org
 // Syntax:        C++11
