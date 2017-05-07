d0928c9aba3406cbcf30ca13d77c65854f8bcdb6
peterzhu9210 <ezhu2@stevens.edu>
2017-05-07 16:03:23 -0400

    Stores a MidiMessage

--- release/include/midifile/MidiEvent.h
+++ release/include/midifile/MidiEvent.h
@@ -1,3 +1,6 @@
+// Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
+// Creation Date: Mon Feb 16 12:26:32 PST 2015 Adapted from binasc program.
+// Last Modified: Wed Feb 18 14:48:21 PST 2015
 // Filename:      midifile/include/MidiEvent.h
 // Website:       http://midifile.sapp.org
 // Syntax:        C++11
