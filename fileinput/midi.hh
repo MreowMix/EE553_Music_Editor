f0bca37f834abdf7c0e14c05a06bfbcb12b56388
peterzhu9210 <ezhu2@stevens.edu>
2017-05-07 16:37:14 -0400

    List of a Midi file

--- fileinput/midi.hh
+++ fileinput/midi.hh
@@ -7,16 +7,21 @@
 using namespace std;
 
 
-
+//get Header Chunk Data
 vector<unsigned char> Head_Data;
-
-
+//get Track Chunk Data
 vector<unsigned char> Track_Data;
+//get Track Length
 vector<unsigned char> Track_Length;
+//get Track Event
 vector<unsigned char> Track_event;
+//get Meta List
 vector<unsigned char> Meta_Type;
+//get Meta Length
 vector<unsigned char> Meta_Length;
+//get Meta Event
 vector<unsigned char> Meta_event;
+//get Notation
 vector<unsigned char> Nota_event;
 
 
