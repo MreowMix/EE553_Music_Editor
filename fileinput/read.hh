567695f40f4bf4cabcc75b7ff675bd61bbcaacc3
peterzhu9210 <ezhu2@stevens.edu>
2017-05-07 18:49:56 -0400

    Read and Create Function

--- fileinput/read.hh
+++ fileinput/read.hh
@@ -1,7 +1,10 @@
 #ifndef READ_HH
 #define READ_HH
 
+// Because i need to read a midi file and print out the notation
 void Read(const char* filename,int file_size);
+
+//Because i need to create a new midi file for furture use
 void Create(const char* filename);
 
 #endif // READ_HH
